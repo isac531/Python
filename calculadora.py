b8d6097776497d4bb92a36dbcdcc60fb5e518a96
isac531 <66953921+isac531@users.noreply.github.com>
2020-06-18 16:31:42 -0300

    Calculadora de dois dígitos em Python

---
+++ calculadora.py
@@ -0,0 +1,28 @@
+z=0
+retry = 0
+exec=1
+while exec == 1:
+
+    operacao = int(input("Escolha a operação desejada: \n 1 - Adição \n 2 - Subtração \n 3 - Multiplicação \n 4 - Divisão \n R="))
+
+    x = int(input("Digite o Primeiro digito: \n"))
+    y = int(input("Digite o Segundo digito: \n"))
+
+    if operacao == 1:
+        z = x + y
+    if operacao == 2:
+        z = x - y
+    if operacao == 3:
+        z = x * y
+    if operacao == 4:
+        z = x / y
+
+    print("O Resultado da sua conta é: ")
+    print(z)
+    retry = int(input('Realizar outra operação? \n 1 - Sim \n 2 - Não \n R= '))
+    if retry == 1:
+        exec = 1
+
+    if retry == 2:
+        exec = 0
+        quit()
