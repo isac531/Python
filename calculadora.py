9ea3137dd1e94de0a58fef4338e41b1edfd7ea12
isac531 <66953921+isac531@users.noreply.github.com>
2020-06-18 19:58:09 -0300

    Calculadora de dois dígitos em Python

--- calculadora.py
+++ calculadora.py
@@ -3,32 +3,34 @@ retry = 0
 exec=1
 while exec == 1:
 
-    operacao = int(input("Escolha a operação desejada: \n 1 - Adição \n 2 - Subtração \n 3 - Multiplicação \n 4 - Divisão \n R="))
+    operacao = int(input('Escolha a operação desejada: \n 1 - Adição \n 2 - Subtração \n 3 - Multiplicação \n 4 - Divisão \n R='))
 
-    x = int(input("Digite o Primeiro digito: \n"))
-    y = int(input("Digite o Segundo digito: \n"))
+    if operacao > 4:
+        print('Comando incorreto, tente novamente')
 
-    if operacao == 1:
-        z = x + y
-        print("O Resultado da sua conta é: ")
-        print(x, '+' ,y, '=',z)
-    elif operacao == 2:
-        z = x - y
-        print("O Resultado da sua conta é: ")
-        print(x, '-' ,y, '=' ,z)
-    elif operacao == 3:
-        z = x * y
-        print(x, '*' ,y, '=' ,z)
-    elif operacao == 4:
-        z = x / y
-        print(x, '/' ,y, '=' ,z)
+    else:
+        x = int(input("Digite o Primeiro digito: \n"))
+        y = int(input("Digite o Segundo digito: \n"))
+
+        if operacao == 1:
+            z = x + y
+            print('O Resultado da sua conta é:' ,x, '+' ,y, '=',z)
+        elif operacao == 2:
+            z = x - y
+            print('O Resultado da sua conta é:' ,x, '-' ,y, '=' ,z)
+        elif operacao == 3:
+            z = x * y
+            print('O Resultado da sua conta é:' ,x, '*' ,y, '=' ,z)
+        elif operacao == 4:
+            z = x / y
+            print('O Resultado da sua conta é:' ,x, '/' ,y, '=' ,z)
 
 
 
-    retry = int(input('Realizar outra operação? \n 1 - Sim \n 2 - Não \n R= '))
-    if retry == 1:
-        exec = 1
+        retry = int(input('Realizar outra operação? \n 1 - Sim \n 2 - Não \n R= '))
+        if retry == 1:
+            exec = 1
 
-    if retry == 2:
-        exec = 0
-        quit()
+        if retry == 2:
+            exec = 0
+            quit()
